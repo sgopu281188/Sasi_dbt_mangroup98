973811d2eef7e968886e7285264b01f67a23c2f4
rathnabharathi2396.rb@gmail.com <rathnabharathi2396.rb@gmail.com>
2026-03-08 17:14:33 +0000

    commit_20262

--- models/Stage/Vars.sql
+++ models/Stage/Vars.sql
@@ -1,13 +1,12 @@
 {{
     config(
         materialized = 'table',
-        database = 'PSB_DIS',
-        unique_key = 'employee_no',
+        database = 'COMPANY',
+        unique_key = 'department_id',
         on_schema_change = 'append_new_columns'
     )
 }}
 
 select *
 from
-{{ref('employees')}} where manager_id in ({{ var("years") | join(",") }}) 
-employees where years in var{{years}}
+{{ref('departments')}} where manager_id in ({{ var("manager_ids") | join(",") }}) 
